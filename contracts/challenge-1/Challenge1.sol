4597220275fb9b76d243b778a7b6fde7195ce0dd
Ilan <ilanolkies@outlook.com>
2022-12-16 12:27:20 -0300

    Solve 1

--- contracts/challenge-1/Challenge1.sol
+++ contracts/challenge-1/Challenge1.sol
@@ -26,6 +26,12 @@ contract Challenge1 {
       } 
 
       // Write your code here
+      let s := sub(mul(b, b), mul(mul(4, a), c))
+      let num1 := add(sub(0, b), s)
+      let num2 := sub(sub(0, b), s)
+
+      x1 := div(num1, mul(2, a))
+      x2 := div(num1, mul(2, a))
     }
 
     return (x1,x2);
